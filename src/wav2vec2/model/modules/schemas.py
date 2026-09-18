edcbbf01d32553289b789c97c60df7d57577d690
nagohachi <nagohachi.study@gmail.com>
2026-09-18 22:50:32 +0900

    added schemas

--- src/wav2vec2/model/modules/schemas.py
+++ src/wav2vec2/model/modules/schemas.py
@@ -7,3 +7,17 @@ class Wav2Vec2FeatureEncoderConfig:
     out_channels: tuple[int, ...] = (512, 512, 512, 512, 512, 512, 512)
     kernel_sizes: tuple[int, ...] = (10, 3, 3, 3, 3, 2, 2)
     strides: tuple[int, ...] = (5, 2, 2, 2, 2, 2, 2)
+
+
+@dataclass(frozen=True)
+class Wav2Vec2AdapterConfig:
+    in_dim: int = 512
+    out_dim: int = 768
+
+
+@dataclass(frozen=True)
+class Wav2Vec2TransformerConfig:
+    hidden_dim: int = 768
+    ffn_hidden_dim: int = 3072
+    num_layers: int = 12
+    num_attention_heads: int = 8
