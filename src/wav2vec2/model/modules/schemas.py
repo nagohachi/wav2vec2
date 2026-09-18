a31e5fa1cd61d9b975a0c260e6ae64c906ed0d31
nagohachi <nagohachi.study@gmail.com>
2026-09-19 01:30:26 +0900

    added quantization config

--- src/wav2vec2/model/modules/schemas.py
+++ src/wav2vec2/model/modules/schemas.py
@@ -21,3 +21,17 @@ class Wav2Vec2TransformerConfig:
     ffn_hidden_dim: int = 3072
     num_layers: int = 12
     num_attention_heads: int = 8
+
+
+@dataclass(frozen=True)
+class Wav2Vec2QuantizationConfig:
+    num_code_group: int = 2  # G
+    codebook_size_per_group: int = 320  # V
+    concat_codebook_dim: int = 256  # d, codebook hidden dim = d/G
+
+    quantization_in_dim: int = 512  # must be feature enc's output dim
+    quantization_out_dim: int = 768  # must be transformer's hidden dim
+
+    def __post_init__(self) -> None:
+        if self.concat_codebook_dim % self.num_code_group != 0:
+            raise ValueError("concat_codebook_dim must be divisible by num_code_group")
