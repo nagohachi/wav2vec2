ad65f7c34b1f3011adbe0959faa74c6fe85663cc
nagohachi <nagohachi.study@gmail.com>
2026-09-22 02:26:29 +0800

    modified schemas

--- src/wav2vec2/model/schemas.py
+++ src/wav2vec2/model/schemas.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from .modules.schemas import (
     Wav2Vec2AdapterConfig,
@@ -9,8 +9,14 @@ from .modules.schemas import (
 
 @dataclass(frozen=True)
 class Wav2Vec2Config:
-    feature_encoder_config: Wav2Vec2FeatureEncoderConfig
-    adapter_config: Wav2Vec2AdapterConfig
-    transformer_config: Wav2Vec2TransformerConfig
+    feature_encoder_config: Wav2Vec2FeatureEncoderConfig = field(
+        default_factory=Wav2Vec2FeatureEncoderConfig
+    )
+    adapter_config: Wav2Vec2AdapterConfig = field(
+        default_factory=Wav2Vec2AdapterConfig
+    )
+    transformer_config: Wav2Vec2TransformerConfig = field(
+        default_factory=Wav2Vec2TransformerConfig
+    )
 
     feature_encoder_dropout_prob: float = 0.1
